c60a1cc7249d33304c82fcfa78a543ff23c54e1c
LifeGiver13 <lifefotsing@gmail.com>
2026-01-12 10:28:41 +0100

    Exportation of app for ercel deployment

--- api/index.js
+++ api/index.js
@@ -12,8 +12,11 @@ app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use('/api', animeData);
 
-if (process.env.RENDER === 'true' || process.env.VERCEL !== '1') {
+if (process.env.NODE_ENV !== "production") {
+  const port = 3000;
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
 }
+
+export default app;
